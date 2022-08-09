ce55cecc148d842b29afb50a1838b544b866c385
olidernas4 <nas451@hotmail.es>
2022-08-09 11:22:28 -0500

    ejercicio1phyton

---
+++ ejercicio1phyton1.py
@@ -0,0 +1,22 @@
+from errno import EADDRNOTAVAIL
+from optparse import AmbiguousOptionError
+
+
+nombre = "andres olider"
+apellido = "Narvaez Espejo"
+edad = 29
+email = "ejemplo@hotmail.com"
+telefono = 3216549877
+casado = False
+conhijos = False
+Amigos  = ["andres","juan","natalia"]
+peliculavistas = {
+    1:'avenger',
+    2:'dead pool',
+    3:'interestelar',
+}
+print (nombre,",",apellido,",",edad,",",email,",",telefono)
+print (casado)
+print (conhijos)
+print (Amigos)
+print (peliculavistas)
